fdb2be3cb01c4b970fa1a815f9770fbc0bf5ce75
Harsh Deep <hdeep61034@gmail.com>
2023-12-09 23:38:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello, this is the Node.js and Express version of your Flask app!");
+  res.send("Hello, this is backend for FliprAi Project: Post on /api to get response from chatGPT.");
 });
 
 app.post("/api", async (req, res) => {
